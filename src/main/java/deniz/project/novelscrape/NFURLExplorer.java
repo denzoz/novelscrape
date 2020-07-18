1fc95269ec9f5076ffe640f8969fd09327376b3f
denzoz <none>
2020-07-18 21:01:42 +0100

    Output still same but added more code, some in comments

--- src/main/java/deniz/project/novelscrape/NFURLExplorer.java
+++ src/main/java/deniz/project/novelscrape/NFURLExplorer.java
@@ -9,8 +9,10 @@ import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
 import org.jsoup.nodes.TextNode;
 import java.util.List;
+import org.jsoup.nodes.Node;
 
 
 public class NFURLExplorer {
@@ -20,12 +22,22 @@ public class NFURLExplorer {
         String targetURL = x.getSelectionURL();
         try {
             Document doc = Jsoup.connect(targetURL).get();
-            Elements synopsisBox = doc.getElementsByClass("desc-text");
-            List<TextNode> synopsisTextNodes = synopsisBox.textNodes();
-            for (TextNode node : synopsisTextNodes) {
-                System.out.println(node.getWholeText());
+            Element synopsisBox = doc.getElementsByClass("desc-text").get(0);
+            // System.out.println(synopsisBox.text()); works but removes whitespaces
+            //List<TextNode> synopsisTextNodes = synopsisBox.textNodes();
+//            for (TextNode node : synopsisTextNodes) {
+//                System.out.println(node.getWholeText());
+////            }
+//            for (int i = 0; i < synopsisTextNodes.size(); i++) {
+//                String stuff = synopsisTextNodes.get(i).getWholeText();
+//                System.out.println("From " + targetURL);
+//                System.out.println(stuff);
+//            }
+            for (Node child : synopsisBox.childNodes()) {
+                if (child instanceof TextNode) {
+                    System.out.println(((TextNode) child).text());
+                }
             }
-            
         } catch (IOException e) {
             e.printStackTrace();
         }
